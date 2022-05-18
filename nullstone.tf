868ba1367b3ece01f6bc03ba5dc2f4735aa7ad84
Brad Sickles <brad.sickles@gmail.com>
2022-05-18 17:49:40 -0400

    Enabling network access to all endpoint ports

--- nullstone.tf
+++ nullstone.tf
@@ -22,5 +22,6 @@ locals {
   db_admin_secret_id   = data.ns_connection.rabbitmq.outputs.db_admin_secret_id
   db_protocol          = data.ns_connection.rabbitmq.outputs.db_protocol
   db_endpoints         = data.ns_connection.rabbitmq.outputs.db_endpoints
+  db_ports             = distinct([for url in local.db_endpoints : split(":", url)[1]])
   db_security_group_id = data.ns_connection.rabbitmq.outputs.db_security_group_id
 }
