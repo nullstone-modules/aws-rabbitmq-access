2734f9850fb87b2f34b706fd6034490219a40f2b
Brad Sickles <brad.sickles@gmail.com>
2022-05-19 14:03:16 -0400

    Fixed compliance checks

--- security.tf
+++ security.tf
@@ -1,6 +1,7 @@
 resource "aws_security_group_rule" "app-to-datastore" {
   for_each = { for port in local.db_ports : port => port }
 
+  description              = "Allow access from app to RabbitMQ"
   security_group_id        = var.app_metadata["security_group_id"]
   type                     = "egress"
   from_port                = each.value
@@ -12,6 +13,7 @@ resource "aws_security_group_rule" "app-to-datastore" {
 resource "aws_security_group_rule" "datastore-from-app" {
   for_each = { for port in local.db_ports : port => port }
 
+  description              = "Allow access to RabbitMQ from app"
   security_group_id        = local.db_security_group_id
   type                     = "ingress"
   from_port                = each.value
